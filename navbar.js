186812616dfd2843ce5e10858c127b876a404760
Rami <rami15manna@gmail.com>
2016-06-15 15:41:12 -0400

    testing/updating

---
+++ navbar.js
@@ -0,0 +1,49 @@
+document.write('<!DOCTYPE html>\
+<html lang="en">\
+<head>\
+    <meta charset="utf-8">\
+    <title>The Life of Ramroom</title>\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <meta name="description" content="">\
+    <meta name="author" content="">\
+\
+\
+	<!-- Bootstrap: Latest compiled and minified CSS -->\
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">\
+\
+	<!-- jQuery library -->\
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>\
+\
+	<!-- Bootstrap: Latest compiled JavaScript -->\
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>\
+\
+    <!-- CSS styles -->\
+    <link href="style.css" rel="stylesheet">\
+\
+</head>\
+<body>\
+	<div class="navbar navbar-static-top" style="background-color: #512DA8;">\
+		<div class="containter">\
+			<ul class = "nav navbar-nav">\
+				<li><a href="index.html"><span class="glyphicon glyphicon-heart"></span> Love</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-music"></span> Music</a></li>\
+	            <li><a href="projects.html"><span class="glyphicon glyphicon-briefcase"></span> Projects</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-plane"></span> Travel</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-book"></span> Books</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-picture"></span> Photos</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> Art</a></li>\
+\
+	            <li><a href="learning.html"><span class="glyphicon glyphicon-blackboard"></span> Learning</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-cutlery"></span> Cooking</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-knight"></span> Strategy</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-calendar"></span> Calendar</a></li>\
+\
+	            <li><a href="#"><span class="glyphicon glyphicon-user"></span> About</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-earphone"></span> Contact</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-list-alt"></span> R&eacute;sum&eacute;</a></li>\
+	            <li><a href="#"><span class="glyphicon glyphicon-stats"></span> Stats</a></li>\
+\
+	        </ul>\
+	    </div>\
+	</div>\
+');
