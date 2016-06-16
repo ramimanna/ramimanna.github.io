857f142669a660acdf1ee2b390af39449d7ef3ac
Rami <rami15manna@gmail.com>
2016-06-16 13:02:05 -0400

    Name: Project Ramroom

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ document.write('<!DOCTYPE html>\
 <html lang="en">\
 <head>\
     <meta charset="utf-8">\
-    <title>The Life of Ramroom</title>\
+    <title>Project Ramroom</title>\
     <meta name="viewport" content="width=device-width, initial-scale=1.0">\
     <meta name="description" content="">\
     <meta name="author" content="">\
