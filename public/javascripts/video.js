33cab7724250ccd653a73efcb1a9d1e183d7ca69
Rami Manna <piday@mit.edu>
2017-01-23 15:34:47 -0500

    made tracking presentable

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -1,4 +1,3 @@
-console.log("JS AT YO SERVICE BROTHHA");
 var video = document.querySelector("#myVideo");
  
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
